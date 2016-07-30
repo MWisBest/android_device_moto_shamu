b401c986f7603938bf53bce8114207a70cc3708b
Kyle Repinski <repinski23@gmail.com>
2016-07-30 13:03:23 -0500

    Always use WITH_DEXPREOPT, and PIC as well.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -92,13 +92,8 @@ TARGET_HW_DISK_ENCRYPTION := false
 TARGET_CRYPTFS_HW_PATH := device/moto/shamu/cryptfs_hw
 
 # Enable dex-preoptimization to speed up first boot sequence
-ifeq ($(HOST_OS),linux)
-  ifeq ($(TARGET_BUILD_VARIANT),user)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-    endif
-  endif
-endif
+WITH_DEXPREOPT := true
+WITH_DEXPREOPT_PIC := true
 
 TARGET_TOUCHBOOST_FREQUENCY := 1500
 TARGET_USERIMAGES_USE_EXT4 := true
