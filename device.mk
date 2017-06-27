5a4b8893c6b20c0710050f9fa077e5b273cb6279
Kyle Repinski <repinski23@gmail.com>
2017-06-27 01:03:10 -0500

    Don't build modem debugger on userdebug builds.

--- device.mk
+++ device.mk
@@ -288,7 +288,7 @@ PRODUCT_COPY_FILES += \
     device/moto/shamu/nfc/libnfc-brcm-20795a10.conf:system/etc/libnfc-brcm-20795a10.conf
 
 # Modem debugger
-ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+ifneq (,$(filter eng, $(TARGET_BUILD_VARIANT)))
 ifeq (,$(filter aosp_shamu, $(TARGET_PRODUCT)))
 PRODUCT_PACKAGES += \
     QXDMLoggerV2
